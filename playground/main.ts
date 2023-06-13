7fa4bbb89bb1084e91a5aff72aa11a6214a20a8e
wsvaio <wsvaio@qq.com>
2023-06-13 17:24:32 +0800

    perf: 改用pnpm；优化代码；

--- playground/main.ts
+++ playground/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import piniaPluginPersist from "@wsvaio/pinia-plugin-persist";
 import App from "./App.vue";
+
 const pinia = createPinia();
 pinia.use(piniaPluginPersist());
 const app = createApp(App);
--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,59 @@
 import type { PiniaPlugin } from "pinia";
-import { deepPick } from "./deepPick";
+import { pick } from "./utils";
 import type { PersistOption } from "./types";
 
 export default (
-  {
-    key: KEY = "pinia",
-    setter: SETTER = (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-    getter: GETTER = key =>
-      JSON.parse(String(localStorage.getItem(key))) as PersistOption["getter"],
-  } = {} as Pick<PersistOption, "key" | "setter" | "getter">,
+	{
+		key: KEY = "pinia",
+		setter: SETTER = (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+		getter: GETTER = key =>
+			JSON.parse(String(localStorage.getItem(key))) as PersistOption["getter"],
+	} = {} as Pick<PersistOption, "key" | "setter" | "getter">,
 ): PiniaPlugin =>
-  ({ options, store }) => {
-    const persistForEach = (
-      callback: (options: {
-        key: string;
-        paths: string[];
-        setter: PersistOption["setter"];
-        getter: PersistOption["getter"];
-      }) => void,
-    ) => {
-      let { persist } = options;
-      if (!persist) return;
-      if (persist === true) persist = [{}];
-      if (!Array.isArray(persist)) persist = [persist];
+	({ options, store }) => {
+		const persistForEach = (
+			callback: (options: {
+				key: string;
+				paths: string[];
+				setter: PersistOption["setter"];
+				getter: PersistOption["getter"];
+			}) => void,
+		) => {
+			let { persist } = options;
+			if (!persist) return;
+			if (persist === true) persist = [{}];
+			if (!Array.isArray(persist)) persist = [persist];
 
-      persist.forEach(
-        (
-          {
-            key,
-            includes = Object.keys(store.$state),
-            excludes = [],
-            setter = SETTER,
-            getter = GETTER,
-          },
-          index,
-        ) => {
-          key ||= `${KEY}&${store.$id}&${index}`;
-          const paths = includes.filter(item => !excludes.includes(item));
-          callback({ key, paths, setter, getter });
-        },
-      );
-    };
+			persist.forEach(
+				(
+					{
+						key,
+						includes = Object.keys(store.$state),
+						excludes = [],
+						setter = SETTER,
+						getter = GETTER,
+					},
+					index,
+				) => {
+					key ||= `${KEY}&${store.$id}&${index}`;
+					const paths = includes.filter(item => !excludes.includes(item));
+					callback({ key, paths, setter, getter });
+				},
+			);
+		};
 
-    store.$hydrate = () =>
-      persistForEach(({ key, paths, getter, setter }) => {
-        const persisted = getter.call(store, key);
-        persisted
-          ? store.$patch(deepPick(persisted, paths))
-          : setter.call(store, key, deepPick(store.$state, paths));
-      });
-    store.$persist = () =>
-      persistForEach(({ key, paths, setter }) =>
-        setter.call(store, key, deepPick(store.$state, paths)),
-      );
+		store.$hydrate = () =>
+			persistForEach(({ key, paths, getter, setter }) => {
+				const persisted = getter.call(store, key);
+				persisted
+					? store.$patch(pick(persisted, paths))
+					: setter.call(store, key, pick(store.$state, paths));
+			});
+		store.$persist = () =>
+			persistForEach(({ key, paths, setter }) =>
+				setter.call(store, key, pick(store.$state, paths)),
+			);
 
-    store.$hydrate();
-    store.$subscribe(store.$persist);
-  };
+		store.$hydrate();
+		store.$subscribe(store.$persist);
+	};
